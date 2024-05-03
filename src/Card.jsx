c2e434ee3850dac954903e85e415134880ea914c
PG Openda <piusopenda>
2024-05-03 11:16:58 +0300

    finalized card section

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,56 +3,21 @@ import img from './assets/pexels.jpg'
 
 function Card() {
     return (
-        <>
-            <div className='card'>
-                <a href="" className='card-a'>
-                    <img src={img} alt="image" className='card-img'/>
-                    <div className='card-info'>
-                        <h3 className='card-h3'>
-                            Terminal
-                        </h3>
-                        <p className='card-p'>
-                            <span className='card-span'>JavaScript</span>
-                            <span className='card-span'>CSS</span>
-                            <span className='card-span'>Python</span>                        
-                        </p>
-                    </div>
-                </a>
+        
+        <a href="" className='card-a'>
+            <img src={img} alt="image" className='card-img'/>
+            <div className='card-info'>
+                <h3 className='card-h3'>
+                    Terminal
+                </h3>
+                <p className='card-p'>
+                    <span className='card-span'>JavaScript</span>
+                    <span className='card-span'>CSS</span>
+                    <span className='card-span'>Python</span>                        
+                </p>
             </div>
+        </a>
 
-            <div className='card'>
-                <a href="" className='card-a'>
-                    <img src={img} alt="image" className='card-img'/>
-                    <div className='card-info'>
-                        <h3 className='card-h3'>
-                            Terminal
-                        </h3>
-                        <p className='card-p'>
-                            <span className='card-span'>JavaScript</span>
-                            <span className='card-span'>CSS</span>
-                            <span className='card-span'>Python</span>                        
-                        </p>
-                    </div>
-                </a>
-            </div>
-
-            <div className='card'>
-                <a href="" className='card-a'>
-                    <img src={img} alt="image" className='card-img'/>
-                    <div className='card-info'>
-                        <h3 className='card-h3'>
-                            Terminal
-                        </h3>
-                        <p className='card-p'>
-                            <span className='card-span'>JavaScript</span>
-                            <span className='card-span'>CSS</span>
-                            <span className='card-span'>Python</span>                        
-                        </p>
-                    </div>
-                </a>
-            </div>
-
-        </>
     )
 }
 
