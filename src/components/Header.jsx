dd75279146a6008e0419bbf9a8ab78215918ce86
PG Openda <piusopenda>
2024-05-28 12:41:14 +0300

    Addition: Adjustment on different media view ports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,13 @@ function Header() {
     return (
         <>
             <div className='header'>
-            <a href=''>
-                <img className='logo' src={Logo} height={50} />
-            </a>    
-            <h1 className='name'>PG OPENDA</h1>
-            <div className='title-div'>
-                <h3 className='title'>Software Engineer. </h3> 
-            </div>
+                <a href=''>
+                    <img className='logo' src={Logo}  />
+                </a>    
+                <h1 className='name'>PG OPENDA</h1>
+                <div className='title-div'>
+                    <h3 className='title'>Software Engineer. </h3> 
+                </div>
             </div>
         </>
     )
