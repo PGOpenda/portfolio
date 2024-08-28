f79a92deac8db190026825cb44dfd72aa8eccf04
PG Openda <piusopenda@gmail.com>
2024-08-28 12:30:38 +0300

    Footer section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import About  from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <About />
         <Projects />
         <Contact />
+        <Footer />
       </div>
     </main>
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import '../styles/Footer.css'
+
+function Footer() {
+    return (
+        <div className="footer-section">
+            Built by PG Openda
+        </div>
+    )
+}
+
+export default Footer
